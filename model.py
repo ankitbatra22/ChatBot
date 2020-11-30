ce94595d818322e426318eae3553c0c208565c62
ankitbatra22 <ankitbatra2202@gmail.com>
2020-11-30 15:29:40 -0500

    update

--- model.py
+++ model.py
@@ -1,1 +1,27 @@
-print("hello")
+import json
+from toolkit import tokenize, stem, bagOfWords
+
+with open('intents.json', 'r') as f:
+    intents = json.load(f)
+
+all_words = []
+tags = []
+xy = []
+# loop through each sentence in our intents patterns
+for intent in intents['intents']:
+    tag = intent['tag']
+    # add to tag list
+    tags.append(tag)
+    for pattern in intent['patterns']:
+        # tokenize each word in the sentence
+        w = tokenize(pattern)
+        # add to our words list
+        all_words.extend(w)
+        # add to xy pair
+        xy.append((w, tag))
+
+ignore_words = ['?', '.', '!']
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+# remove duplicates and sort
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
--- toolkit.py
+++ toolkit.py
@@ -1,12 +1,14 @@
 import nltk
-#nltk.downlod('punkt')
+#nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
-"""tokenize
-lower and stem
-take out punctiation
-create bag of words"""
+"""
+1. Tokenize (split sentance into array of words / tokens. This includes punctuation.
+2. lower case and find root form of a word
+3. take out punctuation
+4. create bag of words. array with 1 for word that exists in a sentance and 0 for otherwise. 
+"""
 
 def tokenize(sentance):
     return nltk.word_tokenize(sentance)
@@ -17,6 +19,17 @@ def stem(word):
 def bagOfWords(tokenized_sentance, all):
     pass
 
-a = "how long does shipping take?"
-#print(a)
-print(tokenize(a))
+"""a = "how long does shipping take?"
+a = (tokenize(a))
+print(a)
+
+words = ["organize", "organizes", "organizing"]
+
+stemmed_words = []
+for w in words:
+    stemmed = w
+    stemmed_words.append(stem(stemmed))
+
+print(stemmed_words)"""
+
+
